ec5ab55c708314eed0ba60429ffd80c6ebd33f85
刘毅 <liuyi@liuyideiMac.local>
2019-02-15 17:05:11 +0800

    layout

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app-wrapper">
+        <sidebar class="sidebar-container"></sidebar>
+        <div class="main-container">
+            <app-main></app-main>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Sidebar, AppMain } from '@/views/layout/components'
+export default {
+  name: 'layout',
+  components: {
+    Sidebar,
+    AppMain
+  },
+  computed: {
+    // sidebar () {
+    //   return this.$store.state.app.sidebar
+    // }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .app-wrapper {
+        position: relative;
+        height: 100%;
+        width: 100%;
+    }
+</style>
