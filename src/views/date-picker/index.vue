b3b94409e3996945748ef5b7d2476301fe1f7069
刘毅 <liuyi@liuyideiMac.local>
2019-03-04 17:27:29 +0800

    date-picker

--- src/views/date-picker/index.vue
+++ src/views/date-picker/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div class="date-picker-content">
+      <div class="date-picker-input-content">
+        <input type="text" class="date-picker-input" placeholder="日期选择" v-model="date">
+      </div>
   </div>
 </template>
 
@@ -7,12 +10,29 @@
 export default {
   data () {
     return {
-
+      date: '2019 - 3 - 4'
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+  .date-picker-content {
+    .date-picker-input-content {
+      .date-picker-input {
+        border: 1px solid #dcdfe6;
+        color: #606266;
+        display: inline-block;
+        height: 40px;
+        line-height: 40px;
+        width: 200px;
+        border-radius: 4px;
+        background-color: #fff;
+        border: 1px solid #dcdfe6;
+        padding: 0 15px;
+        cursor: pointer;
+      }
+    }
 
+  }
 </style>
