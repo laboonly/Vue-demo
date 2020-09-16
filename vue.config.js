45187aaa815620bab0fe29e20632124893a8c735
刘毅 <326534215@qq.com>
2020-09-16 21:15:02 +0800

    fix: api问题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   devServer: {
-    proxy: 'https://result.eolinker.com/IWVnKePdd79905ba2a21b8dce223816c8439c7913ffd668?uri='
+    proxy: 'https://mockapi.eolinker.com/IWVnKePdd79905ba2a21b8dce223816c8439c7913ffd668/'
   }
 }
